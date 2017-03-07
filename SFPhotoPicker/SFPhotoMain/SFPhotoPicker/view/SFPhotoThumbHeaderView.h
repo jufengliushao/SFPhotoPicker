5a35cf63eee7a1cc1b67e130576d3ed38b82e016
liushaofeng <jufengliushao@163.com>
2017-03-07 10:47:53 +0800

    create thumb header view

---
+++ SFPhotoPicker/SFPhotoMain/SFPhotoPicker/view/SFPhotoThumbHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  SFPhotoThumbHeaderView.h
+//  SFPhotoPicker
+//
+//  Created by cnlive-lsf on 2017/3/7.
+//  Copyright © 2017年 lsf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFPhotoThumbHeaderView : UIView
+
+@end
---
+++ SFPhotoPicker/SFPhotoMain/SFPhotoPicker/view/SFPhotoThumbHeaderView.m
@@ -0,0 +1,21 @@
+//
+//  SFPhotoThumbHeaderView.m
+//  SFPhotoPicker
+//
+//  Created by cnlive-lsf on 2017/3/7.
+//  Copyright © 2017年 lsf. All rights reserved.
+//
+
+#import "SFPhotoThumbHeaderView.h"
+
+@implementation SFPhotoThumbHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
