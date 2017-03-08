238061bb9c9c9f9fcadccdd6f2c8976979b555fb
liushaofeng <jufengliushao@163.com>
2017-03-08 09:20:39 +0800

    add bottom

---
+++ SFPhotoPicker/SFPhotoMain/SFPhotoPicker/view/SFPhotoBottomView.m
@@ -0,0 +1,21 @@
+//
+//  SFPhotoBottomView.m
+//  SFPhotoPicker
+//
+//  Created by cnlive-lsf on 2017/3/8.
+//  Copyright © 2017年 lsf. All rights reserved.
+//
+
+#import "SFPhotoBottomView.h"
+
+@implementation SFPhotoBottomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
