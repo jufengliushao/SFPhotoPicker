c4269db045a1ad62695d180ac9f9c16f1c7e2fab
liushaofeng <jufengliushao@163.com>
2017-03-08 09:34:31 +0800

    bottom init

--- SFPhotoPicker/SFPhotoMain/SFPhotoPicker/view/SFPhotoBottomView.h
+++ SFPhotoPicker/SFPhotoMain/SFPhotoPicker/view/SFPhotoBottomView.h
@@ -3,11 +3,15 @@
 //  SFPhotoPicker
 //
 //  Created by cnlive-lsf on 2017/3/8.
-//  Copyright © 2017年 lsf. All rights reserved.
+//  Copyright © 2017年 . All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface SFPhotoBottomView : UIView
+
+@property (nonatomic, strong) UIButton *editBtn;
+@property (nonatomic, strong) UIButton *origalBtn;
+@property (nonatomic, strong) UIButton *sendBtn;
 
 @end
--- SFPhotoPicker/SFPhotoMain/SFPhotoPicker/view/SFPhotoBottomView.m
+++ SFPhotoPicker/SFPhotoMain/SFPhotoPicker/view/SFPhotoBottomView.m
@@ -9,13 +9,52 @@
 #import "SFPhotoBottomView.h"
 
 @implementation SFPhotoBottomView
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+#pragma mark - system method
+- (instancetype)init{
+    if (self = [super init]) {
+        self.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.6];
+    }
+    return self;
 }
-*/
 
+- (void)drawRect:(CGRect)rect{
+    [super drawRect:rect];
+}
+
+#pragma mark - init
+- (UIButton *)editBtn{
+    if (!_editBtn) {
+        _editBtn = [UIButton buttonWithType:(UIButtonTypeCustom)];
+        [_editBtn setTitle:@"编辑" forState:(UIControlStateNormal | UIControlStateDisabled)];
+        [_editBtn setTitleColor:[UIColor whiteColor] forState:(UIControlStateNormal)];
+        [_editBtn setTitleColor:[UIColor lightGrayColor] forState:(UIControlStateDisabled)];
+        _editBtn.enabled = NO;
+        [self addSubview:_editBtn];
+    }
+    return _editBtn;
+}
+
+- (UIButton *)origalBtn{
+    if (!_origalBtn) {
+        _origalBtn = [UIButton buttonWithType:(UIButtonTypeCustom)];
+        [_origalBtn setTitle:@"原图" forState:(UIControlStateNormal | UIControlStateSelected)];
+        [_origalBtn setTitleColor:[UIColor lightGrayColor] forState:(UIControlStateNormal)];
+        [_origalBtn setTitleColor:[UIColor whiteColor] forState:(UIControlStateSelected)];
+        _origalBtn.selected = NO;
+        [self addSubview:_origalBtn];
+    }
+    return _origalBtn;
+}
+
+- (UIButton *)sendBtn{
+    if (!_sendBtn) {
+        _sendBtn = [UIButton buttonWithType:(UIButtonTypeCustom)];
+        [_sendBtn setTitle:@"发送" forState:(UIControlStateNormal | UIControlStateDisabled)];
+        [_sendBtn setTitleColor:[UIColor lightGrayColor] forState:(UIControlStateDisabled)];
+        [_sendBtn setTitleColor:[UIColor whiteColor] forState:(UIControlStateNormal)];
+        _sendBtn.enabled = NO;
+        [self addSubview:_sendBtn];
+    }
+    return _sendBtn;
+}
 @end
