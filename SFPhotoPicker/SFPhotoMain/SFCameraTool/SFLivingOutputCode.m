cd0fa2bbd3cc2f97dde5f1ead1e4567f34ad8ba3
liushaofeng <jufengliushao@163.com>
2017-03-22 10:55:55 +0800

    add videoToolbox.framework and methods

---
+++ SFPhotoPicker/SFPhotoMain/SFCameraTool/SFLivingOutputCode.m
@@ -0,0 +1,13 @@
+//
+//  SFLivingOutputCode.m
+//  SFPhotoPicker
+//
+//  Created by cnlive-lsf on 2017/3/22.
+//  Copyright © 2017年 lsf. All rights reserved.
+//
+
+#import "SFLivingOutputCode.h"
+
+@implementation SFLivingOutputCode
+
+@end
